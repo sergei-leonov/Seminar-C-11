a5f6ab11d1c9993d7df9512479ab07e5812a5b85
Sergei Leonov <s.andreevih2017@yandex.ru>
2023-02-27 00:04:05 +0300

    Семинар

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿ //int num1 = 25;
+
+//double num2 = 13.45;
+// string word = "Sergei";
+// char sybol = '%';
+// bool check = true
+
+// 1. Напишите программу на в ход 2 чила является ли первое число квадратом второго числа 
+/* 
+
+Console.Write (" imput a first number: ");                // Команда выводит все строку в скобках  <- Запрос пользователя, запрос данных
+int number1 = Convert.ToInt32 (Console.ReadLine());       // (Console.ReadLine()) отвечает за получение инфы с консоля 
+Console.Write (" imput a second number: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+int quad2 = number2 * number2; // хранит квадрат 2 го числа
+
+
+
+if(number1 == quad2)
+    Console.WriteLine("Yes!");
+else
+    Console.WriteLine("No!");
+*/
+
+// Задача с цыклом: Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все числа в промежутке от - N до N
+
+/*
+Console.Write(" imput a posotive number: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + " "); // current + пробел, отделил текущее число от его следующей версии
+    current++; 
+}
+*/
+
+// Целое численное деление или остаток отделения3
+
+/*
+65689 / 10 = 6568
+65689 / 100 = 656
+65689 / 1000 = 65
+65689 / 10000 = 6
+
+65689 % 10 = 9
+65689 % 100 = 89
+65689 % 1000 = 689
+65689 % 10000 = 5689
+
+23456 -> 34
+28563 -> 56
+*/
+
+
+
+
+
+
+
+
