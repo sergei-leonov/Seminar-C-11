58afef5cef818db5b6113a0dd80e75bfbb5fbfda
sergei-leonov <125502254+sergei-leonov@users.noreply.github.com>
2023-02-28 21:08:18 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,63 +1,102 @@
-﻿ //int num1 = 25;
+﻿
+//Task 1: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
 
-//double num2 = 13.45;
-// string word = "Sergei";
-// char sybol = '%';
-// bool check = true
-
-// 1. Напишите программу на в ход 2 чила является ли первое число квадратом второго числа 
-/* 
-
-Console.Write (" imput a first number: ");                // Команда выводит все строку в скобках  <- Запрос пользователя, запрос данных
-int number1 = Convert.ToInt32 (Console.ReadLine());       // (Console.ReadLine()) отвечает за получение инфы с консоля 
-Console.Write (" imput a second number: ");
+/*Console.Write (" imput a first number: ");                                            
+int number1 = Convert.ToInt32 (Console.ReadLine());       
+Console.Write (" imput a second number: ");                                     
 int number2 = Convert.ToInt32 (Console.ReadLine());
-
-int quad2 = number2 * number2; // хранит квадрат 2 го числа
-
-
-
-if(number1 == quad2)
-    Console.WriteLine("Yes!");
-else
-    Console.WriteLine("No!");
+int Max = number1;
+int Min = number2;
+if (number1 > Min)
+        Console.WriteLine ( number1 + "  " + "more" );
+        else 
+        Console.WriteLine ( number2 + "  " + "more" );
+if (number2 > Max)
+        Console.WriteLine ( number1 + "  " + "less" );
+        else 
+        Console.WriteLine ( number2 + "  " + "less" );
 */
 
-// Задача с цыклом: Напишите программу, которая на вход принимает одно число (N),
-// а на выходе показывает все числа в промежутке от - N до N
+ //Task 2: Напишите программу, которая принимает на вход 
+ //три числа и выдаёт максимальное из этих чисел. 
 
-/*
-Console.Write(" imput a posotive number: ");
+/*Console.Write (" imput a first number: ");                                            
+int number1 = Convert.ToInt32 (Console.ReadLine());       
+Console.Write (" imput a second number: ");                                     
+int number2 = Convert.ToInt32 (Console.ReadLine());  
+Console.Write (" imput a third number: ");                                     
+int number3 = Convert.ToInt32 (Console.ReadLine());                   
+
+int Max = number1;
+
+if (number2 > Max)
+Console.WriteLine ("Max" + " " + number2);
+else
+ Console.WriteLine ("Max" + " " + number3);
+*/
+
+ //Task 3: Напишите программу, которая на вход принимает число и выдаёт, 
+ //является ли число чётным (делится ли оно на два без остатка).
+
+/*Console.Write(" output even number: ");
 int num = Convert.ToInt32 (Console.ReadLine());
 
-int current = -num;
+int current = num ;
 
-while(current <= num)
+if ( current % 2 > 0)
+
+   Console.WriteLine("namber" + " " + current + " " + "not even number!" + " " + "and is divisible by 2 with a remainder of 1");   
+else
+    Console.WriteLine("namber" + " " + current + " " + "even number!" + " " + "and is divisible by 2 without a remainder");
+*/
+
+//Task 4:Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+/*Console.Write(" output a number: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+int current = 2;
+
+while (current <= num) 
 {
-    Console.Write(current + " "); // current + пробел, отделил текущее число от его следующей версии
-    current++; 
+        if (current % 2 == 0) 
+
+    Console.WriteLine(current + " ");
+
+current++;
 }
 */
+    
 
-// Целое численное деление или остаток отделения3
+    /*int current = num;
 
-/*
-65689 / 10 = 6568
-65689 / 100 = 656
-65689 / 1000 = 65
-65689 / 10000 = 6
+while(current <= num)
 
-65689 % 10 = 9
-65689 % 100 = 89
-65689 % 1000 = 689
-65689 % 10000 = 5689
+        if ( current % 2 != 1)
+{
+         Console.WriteLine(num + " ");
+}
+        current ++;6
+        */
 
-23456 -> 34
-28563 -> 56
-*/
-
+       
+    
+    
 
 
+
+
+
+   
+
+ 
+
+
+
+
+ 
 
 
 
