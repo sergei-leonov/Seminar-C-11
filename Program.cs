bab0d15e6d5054b1387c68ba9f492a6f0cff7eaf
sergei-leonov <125502254+sergei-leonov@users.noreply.github.com>
2023-03-05 21:47:07 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,74 +1,95 @@
-﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿
+//Task 1: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
 
-//14212 -> нет
-//12821 -> да                    
-//23432 -> да  
-/*    
-int Prompt (string message)
-{
-Console.Write(message);
-int resault = Convert.ToInt32 (Console.ReadLine());
-return resault;
-}
+/*Console.Write (" imput a first number: ");                                            
+int number1 = Convert.ToInt32 (Console.ReadLine());       
+Console.Write (" imput a second number: ");                                     
+int number2 = Convert.ToInt32 (Console.ReadLine());
+int Max = number1;
+int Min = number2;
+if (number1 > Min)
+        Console.WriteLine ( number1 + "  " + "more" );
+        else 
+        Console.WriteLine ( number2 + "  " + "more" );
+if (number2 > Max)
+        Console.WriteLine ( number1 + "  " + "less" );
+        else 
+        Console.WriteLine ( number2 + "  " + "less" );
+*/
 
-int number = Prompt("Введите число");
+ //Task 2: Напишите программу, которая принимает на вход 
+ //три числа и выдаёт максимальное из этих чисел. 
 
-int first = number % 10;            
-int second = number / 10000;
-if (second == first)
+/*Console.Write (" imput a first number: ");                                            
+int number1 = Convert.ToInt32 (Console.ReadLine());       
+Console.Write (" imput a second number: ");                                     
+int number2 = Convert.ToInt32 (Console.ReadLine());  
+Console.Write (" imput a third number: ");                                     
+int number3 = Convert.ToInt32 (Console.ReadLine());                   
 
-      Console.WriteLine("is a palindrome!");
+int Max = number1;
+
+if (number2 > Max)
+Console.WriteLine ("Max" + " " + number2);
 else
-      Console.WriteLine("is not a palindrome!");
+ Console.WriteLine ("Max" + " " + number3);
 */
 
+ //Task 3: Напишите программу, которая на вход принимает число и выдаёт, 
+ //является ли число чётным (делится ли оно на два без остатка).
 
-//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*Console.Write(" output even number: ");
+int num = Convert.ToInt32 (Console.ReadLine());
 
-//A (3,6,8); B (2,1,-7), -> 15.84
+int current = num ;
 
-//A (7,-5, 0); B (1,-1,9) -> 11.53
-/*
-double Distance3D(double x1, double y1, double z1,  double x2, double y2,double z2)
-{
-double res = Math.Sqrt(Math.Pow(x2 - x1 ,2) + Math.Pow(y2 - y1, 2)+ Math.Pow(z2 - z1, 2));
-return Math.Round(res, 2);
-}
+if ( current % 2 > 0)
 
-Console.Write("Введите координату по Х для первой точки ");
-double x1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату по У для первой точки ");
-double y1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату по У для первой точки ");
-double z1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату по Х для второй точки ");
-double x2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату по Y для второй точки ");
-double y2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату по У для первой точки ");
-double z2 = Convert.ToDouble(Console.ReadLine());
-
-double distance = Distance3D(x1, y1, z1, x2, y2, z2);
-System.Console.WriteLine($"Расстояние между точками = {distance}");
+   Console.WriteLine("namber" + " " + current + " " + "not even number!" + " " + "and is divisible by 2 with a remainder of 1");   
+else
+    Console.WriteLine("namber" + " " + current + " " + "even number!" + " " + "and is divisible by 2 without a remainder");
 */
 
+//Task 4:Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
 
-//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-//3 -> 1, 8, 27
-//5 -> 1, 8, 27, 64, 125
-/*
-int Prompt (string message)
+/*Console.Write(" output a number: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+int current = 2;
+
+while (current <= num) 
 {
-Console.Write(message);
-int resault = Convert.ToInt32 (Console.ReadLine());
-return resault;
-}
+        if (current % 2 == 0) 
 
-int number = Prompt("Введите число > ");
-for(int i = 1; i <= number; i++)  
+    Console.WriteLine(current + " ");
 
-{
-Console.Write($"{Math.Pow(i, 3)} ");       
+current++;
 }
 */
+    
+
+    
+
+       
+    
+    
+
+
+
+
+
+   
+
+ 
+
+
+
+
+ 
+
+
+
+
 
